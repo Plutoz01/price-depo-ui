42391721eeeb6bff2729c8a300814d2996efdc6c
László Csatlós <plutoz01@gmail.com>
2017-12-14 18:34:09 +0100

    aot build fix

--- libs/product/src/product.module.ts
+++ libs/product/src/product.module.ts
@@ -1,17 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SecurityModule } from "@price-depo-ui/security";
 import { AuthenticatedGuard } from "@price-depo-ui/security/src/guards/authenticated.guard";
 import { HasAllPermissionsGuard } from "@price-depo-ui/security/src/guards/has-all-permissions.guard";
 import { AdminPageComponent } from './admin/components/admin-page/admin-page.component';
 
 @NgModule( {
-  imports: [ CommonModule,
+  imports: [
+    CommonModule,
+    SecurityModule,
+
     RouterModule.forChild( [
       {
         path: 'admin', component: AdminPageComponent,
         canActivate: [ AuthenticatedGuard, HasAllPermissionsGuard ],
-        data: { [ HasAllPermissionsGuard.permissionsKey ]: [ 'product.admin' ] }
+        data: { permissions: [ 'product.admin' ] }
       }
     ] ) ],
   declarations: [ AdminPageComponent ]
--- libs/profile/src/profile.module.ts
+++ libs/profile/src/profile.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SecurityModule } from "@price-depo-ui/security";
 import { AuthenticatedGuard } from "@price-depo-ui/security/src/guards/authenticated.guard";
 import { HasAllPermissionsGuard } from "@price-depo-ui/security/src/guards/has-all-permissions.guard";
 import { ProfilePageComponent } from './components/profile-page/profile-page.component';
@@ -8,11 +9,12 @@ import { ProfilePageComponent } from './components/profile-page/profile-page.com
 @NgModule( {
   imports: [
     CommonModule,
+    SecurityModule,
     RouterModule.forChild( [
       {
         path: '', pathMatch: 'full', component: ProfilePageComponent,
         canActivate: [ AuthenticatedGuard, HasAllPermissionsGuard ],
-        data: { [ HasAllPermissionsGuard.permissionsKey ]: [ 'profile' ] }
+        data: { permissions: [ 'profile' ] }
       }
     ] )
   ],
